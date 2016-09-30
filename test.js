93e67aa8ebd8d6880e1055d6a6e89f6864a8976a
Simon Eric Scholl <simon.scholl@me.com>
2016-09-30 13:07:25 +0200

    fix error for Timothy book

--- test.js
+++ test.js
@@ -6,3 +6,5 @@ console.log(bible.getBook(36));
 console.log(bible.getBook('Haggai'));
 console.log(bible.searchId("ti"));
 console.log(bible.searchId("prediger"));
+console.log(bible.getBook('2 Timothy').shortcuts);
+console.log(bible.getBook('1 Timothy').shortcuts);
